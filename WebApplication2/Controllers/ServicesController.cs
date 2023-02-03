b7b0c9714aadf7b6c1fb9db6c49b22a4945b1a29
İlker <117676317+ilkersatur@users.noreply.github.com>
2023-02-03 08:57:00 +0300

    Add files via upload

---
+++ WebApplication2/Controllers/ServicesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string username, string password)
+        {
+            var obj= new { Username=username, Password=password };
+
+            HttpClient client = new HttpClient();
+            var result = await client.PostAsJsonAsync("https://localhost:7264/api/Guvenli",obj);
+            string token = result.Content.ReadAsStringAsync().Result;
+
+
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            string json = await client.GetStringAsync("https://localhost:7264/api/Guvenli");
+            //return Content(json);
+            
+            List<Urun> urunler= JsonConvert.DeserializeObject<List<Urun>>(json);
+            return View("SonucGoster",urunler);
+        }
+
+        public IActionResult SonucGoster()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Urun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public partial class Urun
+    {
+        public int UrunId { get; set; }
+        public string? UrunAdi { get; set; }
+        public int KategoriId { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+}
